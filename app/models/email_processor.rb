97647966c045f0e0e1ec9bbfb798f78cd78d13ef
josh bradley <josh@gotostepone.com>
2015-09-25 11:10:03 -0700

    email

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -8,7 +8,7 @@ class EmailProcessor
     # processing reports, etc
     # here's an example of model creation
     user = User.find_or_initialize_by(email: @email.from[:email])
-    a = user.articles.create!(
+    a = user.articles.create(
       title: @email.subject,
       body: @email.body,
       image: @email.attachments.first
@@ -18,12 +18,16 @@ class EmailProcessor
     end
     if @email.raw_text.match('event:').present?
       start_date =  @email.raw_text.match(/start:(.*)/)[1]
-      end_date =  @email.raw_text.match(/end:(.*)/)[1]
+      if @email.raw_text.match('end:').present?
+        end_date =  @email.raw_text.match(/end:(.*)/)[1]
+      end
       a.events.create!(start_date: start_date.to_datetime, end_date: end_date.to_datetime)
     end
     if @email.raw_text.match('location:').present?
-      line_1 = @email.raw_text.match(/line_1:(.*)/)[1]
-      line_1 = @email.raw_text.match(/line_2:(.*)/)[1]
+      line_1 = @email.raw_text.match(/address:(.*)/)[1]
+      if @email.raw_text.match('address_2:').present?
+        line_1 = @email.raw_text.match(/address_2:(.*)/)[1]
+      end
       line_1 = @email.raw_text.match(/city:(.*)/)[1]
       line_1 = @email.raw_text.match(/postal_code:(.*)/)[1]
       a.locations.create!(line_1: line_1, line_2: line_2, city: city, state_or_province: "CA", postal_code: postal_code)
