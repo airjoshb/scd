f1e3e646e0884d2f43aa4ed342c69c5b9e7ee6b7
josh bradley <josh@gotostepone.com>
2015-10-02 15:00:23 -0700

    fix

--- app/models/location.rb
+++ app/models/location.rb
@@ -6,7 +6,7 @@ class Location < ActiveRecord::Base
   after_validation :geocode
 
   def full_address
-    "#{line_1}, #{postal_code}, #{city}, #{state}"
+    "#{line_1}, #{postal_code}, #{city}, #{state_or_province}"
   end
 
 end
