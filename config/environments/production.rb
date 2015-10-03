b28b3aea8b135ddc6afb8c2832e7aab8a39b1cac
josh bradley <josh@gotostepone.com>
2015-10-02 21:36:40 -0700

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,9 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.show_previews = true
+  config.consider_all_requests_local = true
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
@@ -78,15 +81,8 @@ Rails.application.configure do
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
-  config.action_mailer.smtp_settings = {
-    :address   => "smtp.mandrillapp.com",
-    :port      => 25,
-    :user_name => Rails.application.secrets.MANDRILL_USERNAME,
-    :password  => Rails.application.secrets.MANDRILL_API_KEY
-  }
-
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'gotostepone.com' }
+  config.action_mailer.default_url_options = { :host => 'santacruzdaily.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   # Send email in development mode?
