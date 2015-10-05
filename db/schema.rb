1c3cdac39987b620bc51981d0544c5ee8f6b9b67
josh bradley <josh@gotostepone.com>
2015-10-05 11:56:43 -0700

    fix text for tips and why

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921213601) do
+ActiveRecord::Schema.define(version: 20151005185355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 20150921213601) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.string   "tips"
-    t.string   "why"
+    t.text     "tips"
+    t.text     "why"
   end
 
   create_table "events", force: true do |t|
