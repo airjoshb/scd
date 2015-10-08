d381f7afa568928f76b995457a11971da855ef38
josh bradley <josh@gotostepone.com>
2015-10-08 12:24:49 -0700

    feed

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -8,4 +8,25 @@ class VisitorsController < ApplicationController
       format.html { render layout: 'preview' }
     end
   end
+
+  def rss
+    @posts = Article.active
+
+    respond_to do |format|
+      format.xml {
+        render :layout => false
+        headers["Content-Type"] = 'application/rss+xml; charset=utf-8'
+      }
+    end
+  end
+
+  def sitemap
+    respond_to do |format|
+      format.xml {
+        render :layout => false
+        headers["Content-Type"] = 'text/xml'
+      }
+    end
+  end
+
 end
