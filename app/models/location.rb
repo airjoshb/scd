239d09bca5c7a3c233fc712a336fb515c759ff65
josh bradley <josh@gotostepone.com>
2015-10-14 13:48:17 -0700

    fix

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,8 @@ class Location < ActiveRecord::Base
   belongs_to :article
   belongs_to :user
 
+  validates_presence_of :line_1, :postal_code, :city
+
   geocoded_by :full_address
   after_validation :geocode
 
