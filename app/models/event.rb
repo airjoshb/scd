ad9100c978904c777f43ccce02d5fd842408bcca
josh bradley <josh@gotostepone.com>
2015-10-14 13:52:52 -0700

    fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ActiveRecord::Base
   belongs_to :article
 
+    validates_presence_of :start_date
+
+
   scope :active, -> { where('start_date >= ?', Time.current + 3.hours)}
   scope :inactive, -> { where('start_date <= ?', Time.current + 3.hours) }
 
