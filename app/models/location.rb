e120a5aace31f1daac02eca544306fcf5e770d5e
josh bradley <josh@gotostepone.com>
2015-10-14 19:01:31 -0700

    fix

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,7 +5,12 @@ class Location < ActiveRecord::Base
   validates_presence_of :line_1, :postal_code, :city
 
   geocoded_by :full_address
-  after_validation :geocode, if: ->(obj){ obj.full_address.present? and obj.full_address_changed? }
+  after_validation :geocode, if: ->(obj){ obj.full_address.present? and obj.address_changed? }
+
+  def address_changed?
+    attrs = %w(line_1 city state_or_province postal_code)
+    attrs.any?{|a| send "#{a}_changed?"}
+  end
 
   def full_address
     "#{line_1}, #{postal_code}, #{city}, #{state_or_province}"
